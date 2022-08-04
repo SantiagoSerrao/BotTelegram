b9ee3a853efd4828a470fcb3473d9a0003fce6d1
SantiagoSerrao <49415402+SantiagoSerrao@users.noreply.github.com>
2022-08-04 20:14:08 -0300

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -13,9 +13,7 @@ INPUT_TEXT = 0
 def start(update, context):
     logger.info('He recibido un comando start')
     update.message.reply_text('¡Hola %s este es un bot usado con The Hive !' % update.message.from_user.name)
-def chiste(update, context):
-    logger.info('Consultando API Chiste')
-    update.message.reply_text(get_chiste())
+
 def leerAlertas(update,context):
     logger.info('Leyendo ultima alerta')
     update.message.reply_text(read_alerts())
@@ -29,6 +27,8 @@ def alertas(update,context):
             update.message.reply_text(i['description'])
     else:
         update.message.reply_text('No hay alertas nuevas! Tomate un cafecito ' + '\U0001F917')
+        
+# function to promote a case to pish case in the hive platform        
 def alertToPhishCase(update,context):
     logger.info('Promoviendo alerta a un caso')
     update.message.reply_text('Es necesario que me pases el alert id para crear el caso %s.' % update.message.from_user.name)
@@ -42,6 +42,8 @@ def creatPhishCase(update,contenxt):
     update.message.text = ''
     return ConversationHandler.END
 
+
+# function to promote a case to incident case in the hive platform  
 def alertToIncidentCase(update,context):
     logger.info('Promoviendo alerta a un caso')
     update.message.reply_text('Es necesario que me pases el alert id para crear el caso %s.' % update.message.from_user.name)
@@ -57,29 +59,11 @@ def creatIncidentCase(update,contenxt):
 
 
 
-
-def ioc(update, context):
-    logger.info('Dialogo IOC')
-    update.message.reply_text('Es necesario que me pases el mensaje para parsearlo %s.' % update.message.from_user.name)
-    return INPUT_TEXT
-def updateIoc(update, context):
-    logger.info('Se recibio el Text a Parsear')
-    text = update.message.text 
-    direcciones_ip = get_ips(text)
-    hashes = get_hash(text)
-    if len(direcciones_ip) != 0:
-        logger.info('Las Direcciones IP: %s' % direcciones_ip)
-        update.message.reply_text('Se recibio el IoC, procederemos a aplicar los siguientes cambios.Direcciones IPs: %s' % direcciones_ip)
-    if len(hashes) != 0:
-        logger.info('Los Hash: %s' % hashes)
-        update.message.reply_text('Se recibio el IoC, procederemos a aplicar los siguientes cambios. Hashes: %s' % hashes)
-    return ConversationHandler.END
-
 """ Main del Programa """
 if __name__ == '__main__':
 
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    logger = logging.getLogger('nAutomaticBot')
+    logger = logging.getLogger('Hive_Bot')
 
     """ Llave API para conectarse a Telegram """
     updater = Updater(token=os.getenv("TOKEN_TELEGRAM"), use_context=True)
@@ -87,7 +71,6 @@ if __name__ == '__main__':
 
     """ Handler's """
     dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(CommandHandler('chiste', chiste))
     dp.add_handler(CommandHandler('leerAlertas',leerAlertas ))
     dp.add_handler(CommandHandler('alertas', alertas ))
     dp.add_handler(ConversationHandler(
